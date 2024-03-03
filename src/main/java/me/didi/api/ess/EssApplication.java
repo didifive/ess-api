b7e1ea5a53b8f5126018bb914601df8894a8120b
Luis Zancanela <luisczdidi@gmail.com>
2024-03-03 15:50:30 -0300

    init java project
    Init Java Project with https://start.spring.io/

---
+++ src/main/java/me/didi/api/ess/EssApplication.java
@@ -0,0 +1,13 @@
+package me.didi.api.ess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/didi/api/ess/EssApplicationTests.java
@@ -0,0 +1,14 @@
+package me.didi.api.ess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssApplicationTests {
+
+	@Test
+	void contextLoads() {
+		EssApplication.main(new String[] {});
+	}
+
+}
