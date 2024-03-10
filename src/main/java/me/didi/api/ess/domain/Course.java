1ccd0ed8b988b18273e2127785ac77be60200efe
Luis Zancanela <luisczdidi@gmail.com>
2024-03-10 10:48:48 -0300

    feat: update Course domain

--- src/main/java/me/didi/api/ess/domain/Course.java
+++ src/main/java/me/didi/api/ess/domain/Course.java
@@ -1,4 +1,48 @@
 package me.didi.api.ess.domain;
 
-public record Course() {
+import me.didi.api.ess.domain.enums.Frequency;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+public record Course(UUID id,
+                     String name,
+                     Frequency frequency,
+                     String period,
+                     LocalDate initDate,
+                     LocalDate recoveryDate,
+                     LocalDate endDate,
+                     Set<Student> students,
+                     Set<Subject> subjects,
+                     Set<Message> messages,
+                     Set<News> news,
+                     Set<Shortcut> shortcuts
+) {
+    public static Course createNew(String name,
+                                Frequency frequency,
+                                String period,
+                                LocalDate initDate,
+                                LocalDate recoveryDate,
+                                LocalDate endDate,
+                                Set<Student> students,
+                                Set<Subject> subjects,
+                                Set<Message> messages,
+                                Set<News> news,
+                                Set<Shortcut> shortcuts) {
+        return new Course(
+                UUID.randomUUID(),
+                name,
+                frequency,
+                period,
+                initDate,
+                recoveryDate,
+                endDate,
+                students,
+                subjects,
+                messages,
+                news,
+                shortcuts
+        );
+    }
 }
